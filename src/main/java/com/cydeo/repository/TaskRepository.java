ec5a3c1c614a1eb8add76c1b5dea68ee94f90918
Agabeg8 <agabegh@gmail.com>
2022-11-25 11:28:00 +0100

    Task Service imp

--- src/main/java/com/cydeo/repository/TaskRepository.java
+++ src/main/java/com/cydeo/repository/TaskRepository.java
@@ -1,8 +1,11 @@
 package com.cydeo.repository;
 
+import com.cydeo.entity.Project;
 import com.cydeo.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TaskRepository extends JpaRepository<Task,Long> {
 
@@ -16,4 +19,11 @@ public interface TaskRepository extends JpaRepository<Task,Long> {
     int totalCompletedTasks(String projectCode);
 
 
+    List<Task> findAllByProject(Project project);
 }
+
+
+
+
+
+
